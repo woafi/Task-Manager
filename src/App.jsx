d78a19b9c98cd756038df30118b6c5a52bf50c8e
Mohammad Woafi <woafisun@yahoo.com>
2025-02-23 11:15:41 +0600

    version 1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,127 @@
+import { useEffect, useState } from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+  const [editIndex, setEditIndex] = useState(null) // Track which task is being edited
+  const [editText, setEditText] = useState("") // Store edited text
+  const [showFinished, setshowFinished] = useState(true)
+
+  useEffect(() => {
+    const todoString = localStorage.getItem("todos")
+    if (todoString) {
+      setTodos(JSON.parse(todoString)) // Load saved todos
+    }
+  }, [])
+
+  const toggleFinished = (params) => {
+    setshowFinished(!showFinished)
+  }
+
+  const handleAdd = () => {
+    if (todo.trim() === "") return;
+    const newTodos = [...todos, { todo, isCompleted: false }]
+    setTodos(newTodos)
+    setTodo("")
+    saveTodo(newTodos)
+  }
+
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+  }
+
+  const handleCheckbox = (index) => {
+    const updatedTodos = todos.map((item, i) =>
+      i === index ? { ...item, isCompleted: !item.isCompleted } : item
+    )
+    setTodos(updatedTodos)
+    saveTodo(updatedTodos)
+  }
+
+  const handleEdit = (index, text) => {
+    setEditIndex(index)
+    setEditText(text)
+  }
+
+  const handleSave = (index) => {
+    const updatedTodos = [...todos]
+    updatedTodos[index].todo = editText
+    setTodos(updatedTodos)
+    setEditIndex(null)
+    saveTodo(updatedTodos)
+  }
+
+  const handleDelete = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index)
+    setTodos(updatedTodos)
+    saveTodo(updatedTodos)
+  }
+
+  const saveTodo = (todosToSave) => {
+    localStorage.setItem("todos", JSON.stringify(todosToSave))
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="container bg-blue-100 rounded-xl my-7 mx-auto p-5 min-h-[80vh]">
+        <h2 className='text-3xl font-bold logoTodo text-grey mb-3'>ToDo List</h2>
+
+        <div className="addTodo flex">
+          <input type="text" value={todo} onChange={handleChange} placeholder='Task Title' className='bg-white w-1/2 rounded-md px-3' />
+          <button onClick={handleAdd} className='bg-blue-400 hover:bg-blue-500 text-white p-3 py-1 rounded-md mx-6 font-bold cursor-pointer'>Add</button>
+        </div>
+        <div className='h-[1px] bg-black my-5 opacity-15 mx-auto'></div>
+        <div className='finish-box flex items-center mt-6 justify-between w-4/5'>
+          <h2 className='font-bold text-xl text-grey cursor-pointer'>List of Tasks</h2>
+          <div className='finishedTask font-bold text-lime-950'>
+            <input type="checkbox" onChange={toggleFinished} checked={showFinished} />
+            <span className='mx-1'>Show Finished Task</span>
+          </div>
+        </div>
+
+        <div className="display pl-5 ">
+          {todos.length === 0 && <div className='my-2'> Task is not added yet </div>}
+          {todos.map((item, index) => {
+            return (showFinished || !item.isCompleted) && <div key={index} className='task-box  flex w-4/5 justify-between my-2 items-center'>
+              
+              <input type="checkbox" checked={item.isCompleted} onChange={() => handleCheckbox(index)} />
+              {/* condition */}
+              {editIndex === index ? (
+                <input type="text" value={editText} onChange={(e) => setEditText(e.target.value)} className="bg-white w-2/3 px-2 rounded edit-tex" />
+              ) : (
+                <div className="text-box w-2/3 overflow-hidden ">
+                  <div className={item.isCompleted ? "line-through" : ""}>
+                    {item.todo}
+                  </div>
+                </div>
+              )}
+
+
+              <div className='buttons '>
+                {/* condition */}
+                {editIndex === index ? (
+                  <button onClick={() => handleSave(index)} className='bg-green-500 hover:bg-green-600 text-white p-3 py-1 rounded-md mx-2 font-bold cursor-pointer'>
+                    Save
+                  </button>
+                ) : (
+                  <button onClick={() => handleEdit(index, item.todo)} className='bg-blue-400 hover:bg-blue-500 text-white p-3 py-1 rounded-md mx-2 font-bold cursor-pointer'>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                  </button>
+                )}
+
+                <button onClick={() => handleDelete(index)} className='bg-red-500 hover:bg-red-600 text-white p-3 py-1 rounded-md mx-1 font-bold cursor-pointer'>
+                <i class="fa-solid fa-trash"></i>
+                </button>
+              </div>
+            </div>
+})}
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div className='flex justify-between bg-blue-500 text-white items-center'>
+      <div className="logo"><span className='logo-text font-bold my-2 text-3xl whitespace-nowrap ml-5 '>Task Manger</span></div>
+      <ul className='mx-6 flex mx-6 my-2'>
+        <li className='navWord cursor-pointer hover:font-bold text-center'>Home</li>
+        <li className='navWord cursor-pointer hover:font-bold text-center'>About Us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
