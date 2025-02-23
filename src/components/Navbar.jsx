2a89cc9893f4003fc7ca74647c20c762d9c75df2
Mohammad Woafi <woafisun@yahoo.com>
2025-02-23 11:30:42 +0600

    version 1.0.1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css"
 function Navbar() {
   return (
     <div className='flex justify-between bg-blue-500 text-white items-center'>
-      <div className="logo"><span className='logo-text font-bold my-2 text-3xl whitespace-nowrap ml-5 '>Task Manger</span></div>
+      <div className="logo"><span className='logo-text font-bold my-2 text-3xl whitespace-nowrap ml-5 '>Task Manager</span></div>
       <ul className='mx-6 flex mx-6 my-2'>
         <li className='navWord cursor-pointer hover:font-bold text-center'>Home</li>
         <li className='navWord cursor-pointer hover:font-bold text-center'>About Us</li>
