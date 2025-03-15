895cd9833a637c363f7f588241df657a17dddff7
Mohammad Woafi <woafisun@yahoo.com>
2025-03-15 14:29:46 +0600

    version 1.1.3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
   return (
     <div className='flex justify-between bg-blue-500 text-white items-center'>
       <div className="logo"><span className='logo-text font-bold my-2 text-3xl whitespace-nowrap ml-5 '>Task Manager</span></div>
-      <ul className='mx-6 flex mx-6 my-2'>
+      <ul className='mx-6 flex my-2'>
         <li className='navWord cursor-pointer hover:font-bold text-center'>Home</li>
-        <li className='navWord cursor-pointer hover:font-bold text-center'>About Us</li>
+        <li className='navWord cursor-pointer hover:font-bold text-center'><a href="https://github.com/woafi/Task-Manager">About Us</a></li>
       </ul>
     </div>
   )
